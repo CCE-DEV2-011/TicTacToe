e45bf53fe1bdc2bbe3eb5e0bf1a840d30a63d8ff
hugobrurrer <hugo.brurrer@gmail.com>
2025-10-29 09:56:30 +0100

    1.0 (1000)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.tictactoe"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 1000
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
